d2714bb426bd315433fc090bc58823590de24797
Philip King <ladysaedie@gmail.com>
2025-04-21 11:40:37 +0000

    Uploads edits to Not A Bit

--- _posts/2021-08-21-not-a-bit.d
+++ _posts/2021-08-21-not-a-bit.d
@@ -3,21 +3,21 @@ title: "Not A Bit"
 description: A forgotten opportunity
 date: 2021-08-21
 last_modified_at: 2025-04-21
-categories: [All, Crytpo]
-tags: [bitcoin, wallet, trading, hodl, dca]
+categories: [All, Crypto]
+tags: [bitcoin, wallet, trading, hodl, dca, missed opportunity, finance]
 ---
 
-Yesterday, in my personal documents, I stumbled upon a link to an old Bitcoin wallet which I had opened years ago and had completely forgottwen about. I vaguely recall opening that account but it meant nothing to me. Out of curiosity, I googled the web site. On its landing page in plain font was a notice informing readers that as of January 2020 the company had ceased trading and had closed down. I sent an email to the support email address enquiring about my old account, just in case. I mean, you never know. Maybe I did buy a bit of Bitcoin.
+Yesterday, in my personal documents, I stumbled upon a link to an old Bitcoin wallet which I had opened years ago and had completely forgotten about. I vaguely recall opening that account but it meant nothing to me. Out of curiosity, I googled the website. On its landing page in plain font was a notice informing readers that as of January 2020 the company had ceased trading and had closed down. I sent an email to the support email address enquiring about my old account, just in case. I mean, you never know. Maybe I did buy a bit of Bitcoin.
 
-Within a day I got a reply. My email address was recognised as a Bitcoin wallet account holder and I had opened my account on St Valentine's Day in 2014 and the acount was still open. They would get back to me. I was excited!
+Within a day I got a reply. My email address was recognised as a Bitcoin wallet account holder and I had opened my account on St Valentine's Day in 2014 and the account was still open. They would get back to me. I was excited!
 
 I did some research. Back in early 2014 the price of Bitcoin was around US$300, or ~ZAR2400 back then, and R2400 back then would have been a manageable amount. I was reasonably well-off in 2014. I could have spent that amount of money without flinching.
 
 If not one whole Bitcoin, then perhaps there was a remnant of Bitcoin in the account. Even a small amount of Bitcoin back then would be worth a chunk of change today.
 
-Today (April 2025), Bitcoin is trading at around R1 600 000. A small gift of even $10's worth of Bitcoin would be worth about R48 000 now.
+Today (April 2025), Bitcoin is trading at around R1 600 000. A small gift of even $10 worth of Bitcoin would be worth about R48 000 now.
 
-So, the million-dollar question was this, did I have the presence of mind to buy Bitcoin in 2014, and did my account have even a scrap of Bitcoin in it?
+So, the million-dollar question was this: did I have the presence of mind to buy Bitcoin in 2014, and did my account have even a scrap of Bitcoin in it?
 
 Their reply came the next day. Here it is, verbatim:
 
